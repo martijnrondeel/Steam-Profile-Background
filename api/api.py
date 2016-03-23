9f6596cc3e42b96df3efad8ddfb7ce3871c640fe
Martijn Rondeel <martijnie96@gmail.com>
2016-03-23 16:21:25 +0100

    Add comments & fix app route

--- api/api.py
+++ api/api.py
@@ -12,10 +12,10 @@ ssl_key = ""
 
 
 def getImage(url):
+    """Extracts profile background image URL from inlined CSS property"""
     content = urllib2.urlopen(url).read()
     tree = lxml.html.fromstring(content)
     elements = tree.cssselect('div.has_profile_background')
-    # extract image URL from css
     url = elements[0].get("style")
     url = url.replace("background-image: url( '", "")
     url = url.replace("' );", "")
@@ -23,7 +23,8 @@ def getImage(url):
 
 
 def getGame(url):
-    p = re.compile(ur'\/([0-9]+)\/')  # extract appID from image url
+    """Extracts appID from image URL and looks up appID's game name"""
+    p = re.compile(ur'\/([0-9]+)\/')
     appID = re.findall(p, url)[0]
     appID = appID.replace("'", "")
     response = urllib2.urlopen(
@@ -32,10 +33,11 @@ def getGame(url):
     return data[appID]["data"]["name"]
 
 
-@app.route('/steamBackground')
+@app.route('/api')
 def main():
     data = request.args.get('url', '')
     url = urlparse(data)
+    # check if URL is steam profile URL
     if url.netloc == 'steamcommunity.com':
         image = getImage(data)
         game = getGame(image)
@@ -45,5 +47,5 @@ def main():
         return "", 404
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', debug=False, port=1337,
+    app.run('127.0.0.1', debug=False, port=1337,  # API runs behind nginx proxy, so only listen on local
             ssl_context=(ssl_cert, ssl_key), threaded=True)
